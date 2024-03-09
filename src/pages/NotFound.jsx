1d9fae4ae41396ebc4ad3fc5f2693d197a85cf0c
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2024-03-09 04:58:43 +0100

    just another commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom"
+export default function NotFound() {
+  return (
+    <div className="not-found-container">
+      <div>
+        <div className="not-found-header">
+          <h1>404</h1>
+        </div>
+        <p>Page Not Found</p>
+        <Link to="/"><div className="not-found-button">Return Home</div></Link>
+      </div>
+    </div>
+  )
+}
