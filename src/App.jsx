d60d50ca1c65ed03558db0f13aa8f1e1b808e29d
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2024-03-10 10:45:57 +0100

    just another commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
+import About from "./pages/About"
 import Projects, {loader as projectsLoader} from "./pages/Projects"
 import NotFound from "./pages/NotFound"
 
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
         path: "projects",
         element: <Projects />,
         loader: projectsLoader
+      },
+      {
+        path: "about",
+        element: <About />
       }
     ]
   }
---
+++ src/pages/About.jsx
@@ -0,0 +1,28 @@
+export default function About() {
+  return (
+    <div className="about-container">
+      <img src="images/about-image.jpg" alt="" />
+      <div>
+        <h1>WHO AM I?</h1>
+        <p>
+        Who am I as in who I really am or who am I as in what I do?
+        </p>
+        <p>
+        Since actually telling you who I am is a lot more difficult than just telling you what I do, I’ll just go ahead and assume that you’d like to know what I do and not who I am in a deep philosophical sense.
+        </p>
+        <p>
+        I am a software developer, primarily. I spent most of my time on the frontend when I started software development—like most software developers—but I’ve begun to transition to the backend because I’ve found that I’m a lot better at handling  business logic than I am at thinking about user interfaces.
+        </p>
+        <p>
+        Since the Meta Frontend Developer professional certificate I took had a course titled “Principles of UX Design,” I also know a thing or two about UI/UX design. I hope I adhered to those principles in the design of this website.  
+        </p>
+        <p>
+        As is expected of me, I’ll attempt to make what I do sound grandly inspirational: in my case, it is that I turn great ideas into awesome products; you just give me the chance to prove it to you.
+        </p>
+        <p>
+        Stay tuned, because what starts here might just go on to change the world.
+        </p>
+      </div>
+    </div>
+  )
+}
