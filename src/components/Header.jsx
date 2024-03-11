45b8be228d98e9eb3512755ec167fde3cc419000
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2024-03-11 01:31:17 +0100

    just another commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 export default function Header() {
-  const openMenu = () => {
-    return (
-      <div style={{backgroundColor: "green", width: "100vw", top: 0, left: 0}}>
-        <div>
-          <h1 style={{color: "red"}}>Here We Go</h1>
-        </div>
-      </div>
-    )
-  }
+  // const openMenu = () => {
+  //   return (
+  //     <div style={{backgroundColor: "green", width: "100vw", top: 0, left: 0}}>
+  //       <div>
+  //         <h1 style={{color: "red"}}>Here We Go</h1>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+
   
   return (
-    <header>
-      <p className="hero">SULEI<span className="hero-span">MAN</span></p>
-      <img className="menu" onClick={openMenu} src="images/menu-2.png" alt="Menu" />
-    </header>
+    <>
+      <header>
+        <p className="hero">SULEI<span className="hero-span">MAN</span></p>
+        <img className="menu" src="images/menu-dark.png" alt="Menu" />
+      </header>
+    </>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,42 @@
 import {Outlet} from "react-router-dom"
-import Header from "./Header"
+// import Header from "./Header"
+import { useState } from "react"
 
 export default function Layout() {
+  const [menu, setMenu] = useState(false)
+  const [dark, setDark] = useState(true)
+  const toggleMenu = () => {
+    setMenu(menu => !menu)
+  }
+  const toggleDark = () => {
+    setDark(dark => !dark)
+  }
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <header>
+        <p onClick={toggleDark} className="hero">SULEI<span className="hero-span">MAN</span></p>
+        <img
+        className="menu"
+        onClick={toggleMenu}
+        src={
+          !menu && !dark ?
+          "images/menu-light.png" :
+          !menu && dark ?
+          "images/menu-dark.png" :
+          menu && !dark ?
+          "images/cancel-light.png" :
+          "images/cancel-dark.png"
+        }
+        alt="Menu"
+        />
+      </header>
+      {menu && 
+      <div>
+        Here we go
+      </div>
+      }
+      {menu ? "" : <Outlet />}
     </>
   )
 }
