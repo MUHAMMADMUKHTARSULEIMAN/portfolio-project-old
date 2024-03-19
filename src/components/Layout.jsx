89862fb576b4f3bc57cda4a4cae2582275a35dba
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2024-03-19 08:09:16 +0100

    just another commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,9 @@
 import { useEffect} from "react"
-import gsap from "gsap";
-import {useGSAP} from "@gsap/react"
 import {NavLink, Outlet} from "react-router-dom"
 import { useState } from "react"
 import useLocalStorage from "use-local-storage"
 
 export default function Layout() {
-  gsap.registerPlugin(useGSAP)
-  useGSAP(() => {
-    const tl = gsap.timeline()
-    // tl.from(".header", {opacity: 0, y: -64, duration: 1, delay: 1, ease: "slow"})
-    tl.from(".hero", {opacity: 0, duration: 1.5, scale: 0.2, delay: 1, ease: "circ.in"})
-    tl.from(".theme-icon", {opacity: 0, duration: 1, ease: "expo.in"}, "+=1")
-    tl.from(".menu", {opacity: 0, duration: 1,scale: 0.2, ease: "expo.in"})
-    tl.from(".home-para", {opacity: 0, duration: 1, scale: 0.2, ease: "expo.in"})
-    tl.from(".home-main", {duration: 1, opacity: 0, scale: 0.2, ease: "expo.in"}, "+=.5")
-    // tl.from(".outlet", {opacity: 0, duration: 2, ease: "circle.inOut"})
-  })
-
-
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   useEffect(() => {
     window.addEventListener('resize', (e) => {
@@ -26,6 +11,7 @@ export default function Layout() {
       setWindowWidth(window.innerWidth);
     });
   }, []);
+
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [darkTheme, setTheme] = useLocalStorage("dark-theme", defaultDark ? true : false);
   const [menu, setMenu] = useState(false)
@@ -42,25 +28,71 @@ export default function Layout() {
         {/* <p className="hero">SULEI<span className="hero-span">MAN</span></p> */}
         <NavLink to="/" onClick={menu ? toggleMenu : ""}>
           <div className="hero-container">
-            <img
-            src={darkTheme ? "images/hero-dark.png" : "images/hero-light.png"}
-            alt="Logo"
-            className="hero"
-            />       
+            {
+              darkTheme ?
+              <img
+              src={"images/hero-dark.png"}
+              alt="Logo"
+              className="hero"
+              /> :
+              <img
+              src={"images/hero-light.png"}
+              alt="Logo"
+              className="hero"
+              />
+            }
           </div>
 
         </NavLink>
         {
           windowWidth < 750 ?
           <div className="mobile-nav">
-            <img
-            className="theme-icon"
-            onClick={switchTheme}
-            src={darkTheme ? "images/light-mode.png" : "images/dark-mode.png"}
-            alt="Theme icon"
-            />
+            {
+              darkTheme ?
+              <img
+              className="theme-icon"
+              onClick={switchTheme}
+              src={"images/light-mode.png"}
+              alt="Theme icon"
+              /> :
+              <img
+              className="theme-icon"
+              onClick={switchTheme}
+              src={"images/dark-mode.png"}
+              alt="Theme icon"
+              />
+            }
 
-            <img
+            {
+              !menu && !darkTheme ?
+              <img
+              className="menu"
+              onClick={toggleMenu}
+              src={"images/menu-light.png"}
+              alt="Menu"
+              /> :
+              !menu && darkTheme ?
+              <img
+              className="menu"
+              onClick={toggleMenu}
+              src={"images/menu-dark.png"}
+              alt="Menu"
+              /> :
+              menu && !darkTheme ?
+              <img
+              className="menu"
+              onClick={toggleMenu}
+              src={"images/cancel-light.png"}
+              alt="Menu"
+              /> :
+              <img
+              className="menu"
+              onClick={toggleMenu}
+              src={"images/cancel-dark.png"}
+              alt="Menu"
+              />
+            }
+            {/* <img
             className="menu"
             onClick={toggleMenu}
             src={
@@ -73,7 +105,7 @@ export default function Layout() {
               "images/cancel-dark.png"
             }
             alt="Menu"
-            />
+            /> */}
           </div> :
           <div className="desktop-nav">
             <img
@@ -95,19 +127,19 @@ export default function Layout() {
         }
       </header>
       {menu && windowWidth < 750 &&
-      <div className="menu-container">
-        <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="/about">
-          <p>ABOUT</p>
-        </NavLink>
-        <hr/>
-        <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="projects">
-          <p>PROJECTS</p>
-        </NavLink>
-        <hr/>
-        <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="contact">
-          <p>CONTACT</p>
-        </NavLink>
-      </div>
+        <div className="menu-container">
+          <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="/about">
+            <p>ABOUT</p>
+          </NavLink>
+          <hr/>
+          <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="projects">
+            <p>PROJECTS</p>
+          </NavLink>
+          <hr/>
+          <NavLink onClick={menu ? toggleMenu : ""} className={({isActive}) => isActive ? "active-link" : ""} to="contact">
+            <p>CONTACT</p>
+          </NavLink>
+        </div>
       }
       {menu && windowWidth < 750 ? "" : <div className="outlet"><Outlet/></div>}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import gsap from "gsap";
-import {useGSAP} from "@gsap/react"
+// import gsap from "gsap";
+// import {useGSAP} from "@gsap/react"
 export default function Home() {
-  gsap.registerPlugin(useGSAP)
-  useGSAP(() => {
-    // const tl = gsap.timeline()
-    // tl.from(".home-para", {duration: 1, ease: "expo.in"})
-    // tl.from(".home-main", {duration: 1, opacity: 0, ease: "expo.in"}, "+=.5")
-  })
+  // gsap.registerPlugin(useGSAP)
+  // useGSAP(() => {
+  //   const tl = gsap.timeline()
+  //   tl.from(".hero", {opacity: 0, duration: 1.5, delay: 1, ease: "circ.in"})
+  //   tl.from(".theme-icon", {opacity: 0, duration: 1, ease: "expo.in"}, "+=1")
+  //   tl.from(".menu", {opacity: 0, duration: 1, ease: "expo"})
+  //   tl.from(".home-para", {opacity: 0, duration: 1, ease: "expo.in"})
+  //   tl.from(".home-main", {duration: 1, opacity: 0, ease: "expo.in"}, "+=.5")
+  // }, {dependencies: []})
   return (
     <main className="home">
       <div>
