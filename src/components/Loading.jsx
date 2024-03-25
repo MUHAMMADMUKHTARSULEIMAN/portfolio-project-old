1a1a82a5cd66ce7445ba3c222e1ce0a5e8b508cf
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2024-03-25 09:36:59 +0100

    just another commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  <div className="loading-container">
+    <div className="loading-sub-container">
+      <h1>S</h1>
+      <h1>L</h1>
+    </div>
+    <div className="loading-sub-container">
+      <h1>M</h1>
+      <h1>N</h1>
+    </div>
+  </div>
+}
